363c030479a95c4c3e8d9ddc509e9e1dfddc68ff
aristoo-11 <mohamed.gherabli@etu.univ-batna2.dz>
2024-06-15 02:35:38 +0100

    fisrt route,env

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require("express");
+Port=3000;
 const app=express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+app.use(express.json());
+dotenv.config();
+mongoose.connect('mongodb+srv://mohamed:rosmouh@cluster0.fdzeqkv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{
+  console.log("database connected");  
+})
+.catch((err)=>{
+  console.log(err);
+})
+const ClinicRoute=require('./Router/ClinicRoute')
+app.use('/clinic',ClinicRoute)
 
+
+
+
+
+
+
+
+
+app.listen(Port,()=>{
+  console.log(`http://localhost:${port}`)
+})
 
 
 
