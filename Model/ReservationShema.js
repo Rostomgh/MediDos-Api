d29c6b01c62ec2df2e1730ed2c1aa76100687b52
aristoo-11 <mohamed.gherabli@etu.univ-batna2.dz>
2024-07-19 03:07:45 +0100

    edit model

--- Model/ReservationShema.js
+++ Model/ReservationShema.js
@@ -3,12 +3,10 @@ const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
 const reservationSchema=new Schema({
   name:{type:String,required:true},
-  email:{type:String,required:true},
   phone:{type:String,required:true},
   date:{type:String,required:true},
   time:{type:String,required:true},
   number:{type:Number,required:true},
-  address:{type:String,required:true},
   })
 
 const reservationModel=mongoose.model("Reservation",reservationSchema);
